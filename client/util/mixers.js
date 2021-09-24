934ff9c7ffd7ffde9ec9be41a11dfd784806c494
hifromkevin <lam.kevin86@gmail.com>
2021-09-23 17:32:04 -0700

    Adds fake data

---
+++ client/util/mixers.js
@@ -0,0 +1,95 @@
+const cocktails = [
+  {
+    cocktailName: 'Vodka Cranberry',
+    cocktailDescription: 'description goes here',
+    cocktailImage: 'img url to go here',
+    ingredients: [
+      {
+        ingredientName: 'vodka',
+        amountInmL: 200
+      },
+      {
+        ingredientName: 'cranberry',
+        amountInmL: 100
+      }
+    ]
+  },
+  {
+    cocktailName: 'Tequila Sunrise',
+    cocktailDescription: 'description goes here',
+    cocktailImage: 'img url to go here',
+    ingredients: [
+      {
+        ingredientName: 'tequila',
+        amountInmL: 100
+      },
+      {
+        ingredientName: 'orangeJuice',
+        amountInmL: 200
+      }
+    ]
+  },
+  {
+    cocktailName: 'Gin and Tonic',
+    cocktailDescription: 'description goes here',
+    cocktailImage: 'img url to go here',
+    ingredients: [
+      {
+        ingredientName: 'gin',
+        amountInmL: 300
+      },
+      {
+        ingredientName: 'tonic',
+        amountInmL: 50
+      }
+    ]
+  },
+  {
+    cocktailName: 'Rum and Coke',
+    cocktailDescription: 'description goes here',
+    cocktailImage: 'img url to go here',
+    ingredients: [
+      {
+        ingredientName: 'rum',
+        amountInmL: 150
+      },
+      {
+        ingredientName: 'coke',
+        amountInmL: 50
+      }
+    ]
+  },
+  {
+    cocktailName: 'Long Island',
+    cocktailDescription: 'description goes here',
+    cocktailImage: 'img url to go here',
+    ingredients: [
+      {
+        ingredientName: 'gin',
+        amountInmL: 15
+      },
+      {
+        ingredientName: 'rum',
+        amountInmL: 15
+      },
+      {
+        ingredientName: 'vodka',
+        amountInmL: 15
+      },
+      {
+        ingredientName: 'tequila',
+        amountInmL: 15
+      },
+      {
+        ingredientName: 'coke',
+        amountInmL: 100
+      },
+      {
+        ingredientName: 'orangeJuice',
+        amountInmL: 30
+      }
+    ]
+  }
+];
+
+export default cocktails;
