74a9d0e4086e094c78af02b9e7d2a749b635076f
hifromkevin <lam.kevin86@gmail.com>
2021-09-26 16:27:15 -0700

    Adds mixer category

--- client/utils/mixers.js
+++ client/utils/mixers.js
@@ -1,33 +1,41 @@
 const mixers = [
   {
+    mixerCategory: 'Vodka',
     mixerName: 'Vodka',
     mixerImage: 'img url to go here'
   },
   {
+    mixerCategory: 'Juice',
     mixerName: 'Cranberry',
     mixerImage: 'img url to go here'
   },
   {
+    mixerCategory: 'Tequila',
     mixerName: 'Tequila',
     mixerImage: 'img url to go here'
   },
   {
+    mixerCategory: 'Juice',
     mixerName: 'Orange Juice',
     mixerImage: 'img url to go here'
   },
   {
+    mixerCategory: 'Gin',
     mixerName: 'Gin',
     mixerImage: 'img url to go here'
   },
   {
+    mixerCategory: 'Tonic',
     mixerName: 'Tonic',
     mixerImage: 'img url to go here'
   },
   {
+    mixerCategory: 'Rum',
     mixerName: 'Rum',
     mixerImage: 'img url to go here'
   },
   {
+    mixerCategory: 'Cola',
     mixerName: 'Coke',
     mixerImage: 'img url to go here'
   }
